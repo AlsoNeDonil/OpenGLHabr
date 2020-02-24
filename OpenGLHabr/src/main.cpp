826b7e73b8aca8f37c8865da14ae9707d2134fa7
NeDonil <gatfild@bk.ru>
2020-02-24 17:58:11 +0300

    Glad

--- OpenGLHabr/src/main.cpp
+++ OpenGLHabr/src/main.cpp
@@ -1,4 +1,5 @@
 #include <GLFW/glfw3.h>
+#include <Glad/glad.h>
 #include <iostream>
 
 int main()
@@ -9,7 +10,19 @@ int main()
 		exit(1);
 	}
 
-	GLFWwindow* window = glfwCreateWindow(100, 100, "Title", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Title", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Lol glad is not ok" << std::endl;
+	}
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glClearColor(0.2f, 0.5f, 0.7f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	system("pause");
 	
--- premake5.lua
+++ premake5.lua
@@ -11,10 +11,13 @@ configurations
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 IncludeDir = {}
 IncludeDir["GLFW"] = "OpenGLHabr/vendor/GLFW/include"
+IncludeDir["Glad"] = "OpenGLHabr/vendor/Glad/include"
 
 include "OpenGLHabr/vendor/GLFW"
+include "OpenGLHabr/vendor/Glad"
 
 project "OpenGLHabr"
+
 location "OpenGLHabr"
 kind "ConsoleApp"
 staticruntime "on"
@@ -34,16 +37,22 @@ objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	{
     "%{prj.name}/src",
     "%{IncludeDir.GLFW}",
+    "%{IncludeDir.Glad}",
     }   
     
     links
     {
       "GLFW",
+      "Glad",
       "opengl32.lib"
     }
 
     filter "system:windows"
 		systemversion "latest"
+		defines
+		{
+			"GLFW_INCLUDE_NONE"
+		}
 
     filter "configurations:Debug"
 		defines "OPENGL_DEBUG"
